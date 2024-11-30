55012ba6fc79d9995c37c99e7331955d76b02d22
PVQuynh <phamquynhltbn12@gmail.com>
2024-12-01 03:34:24 +0700

    fix: sort lesson and part

--- HustLearning_Server/src/main/java/com/example/hust_learning_server/repository/LessonRepository.java
+++ HustLearning_Server/src/main/java/com/example/hust_learning_server/repository/LessonRepository.java
@@ -1,7 +1,9 @@
 package com.example.hust_learning_server.repository;
 
 import com.example.hust_learning_server.entity.Lesson;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +11,39 @@ import java.util.List;
 @Repository
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
 
+    @Query(value = "SELECT * " +
+            "FROM lesson " +
+            "WHERE class_room_id = :classRoomId " +
+            "ORDER BY " +
+            "    SUBSTRING_INDEX(lesson_name, ' ', 1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 2), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 3), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 4), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 5), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 6), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 7), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 8), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 9), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 10), ' ', -1), " +
+            "    SUBSTRING_INDEX(lesson_name, ' ', -1) ",
+            nativeQuery = true)
     List<Lesson> findAllByClassRoomId(long classRoomId);
+
+    @NotNull
+    @Query(value = "SELECT * " +
+            "FROM lesson " +
+            "ORDER BY " +
+            "    SUBSTRING_INDEX(lesson_name, ' ', 1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 2), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 3), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 4), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 5), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 6), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 7), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 8), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 9), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(lesson_name, ' ', 10), ' ', -1), " +
+            "    SUBSTRING_INDEX(lesson_name, ' ', -1) ",
+            nativeQuery = true)
+    List<Lesson> findAll();
 }
--- HustLearning_Server/src/main/java/com/example/hust_learning_server/repository/PartRepository.java
+++ HustLearning_Server/src/main/java/com/example/hust_learning_server/repository/PartRepository.java
@@ -10,10 +10,39 @@ import java.util.List;
 public interface PartRepository extends JpaRepository<Part, Long> {
 
     @NotNull
-    @Query(value = "SELECT * FROM part ORDER BY CAST(SUBSTRING_INDEX(part_name, ' ', -1) AS UNSIGNED) DESC", nativeQuery = true)
+    @Query(value = "SELECT * " +
+            "FROM part " +
+            "ORDER BY " +
+            "    SUBSTRING_INDEX(part_name, ' ', 1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 2), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 3), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 4), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 5), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 6), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 7), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 8), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 9), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 10), ' ', -1), " +
+            "    SUBSTRING_INDEX(part_name, ' ', -1) ",
+            nativeQuery = true)
     List<Part> findAll();
 
-    @Query(value = "SELECT * FROM part WHERE lesson_id = :lessonId ORDER BY CAST(SUBSTRING_INDEX(part_name, ' ', -1) AS UNSIGNED) DESC", nativeQuery = true)
+    @Query(value = "SELECT * " +
+            "FROM part " +
+            "WHERE lesson_id = :lessonId " +
+            "ORDER BY " +
+            "    SUBSTRING_INDEX(part_name, ' ', 1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 2), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 3), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 4), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 5), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 6), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 7), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 8), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 9), ' ', -1), " +
+            "    SUBSTRING_INDEX(SUBSTRING_INDEX(part_name, ' ', 10), ' ', -1), " +
+            "    SUBSTRING_INDEX(part_name, ' ', -1) ",
+            nativeQuery = true)
     List<Part> findAllByLessonId(long lessonId);
 
     boolean existsByPartNameAndLessonId(String partName, long lessonId);
